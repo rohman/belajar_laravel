217be01a3d6506f0dab57efbbf3fc26df84690c0
rohman <rohman.ariel@gmail.com>
2013-10-23 11:08:55 +0700

    update

--- views/authors/view.blade.php
+++ views/authors/view.blade.php
@@ -4,5 +4,6 @@
 	<h1>{{$author->name}}</h1>
 	<p>{{$author->bio}}</p>
 	<small>{{$author->updated_at}}</small>
+	<p>{{HTML::linkRoute('authors','Authors')}} | {{ HTML::linkRoute('editAuthor', 'Edit', array($author->id))}} </p>
 @endsection
 
